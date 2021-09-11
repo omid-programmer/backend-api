e9b401650be17f090814aedfa3a4fa83b6ccdc29
omid zamani <omid.zamani4113367975@gmail.com>
2021-09-11 11:25:10 +0430

    add some controllers-refactor

---
+++ app/Repositories/SubscribeRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Repositories;
+
+use App\Models\Answer;
+use App\Models\Subscribe;
+use App\Models\Thread;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class SubscribeRepository
+{
+    public function getNotifiableUsers($thread_id)
+    {
+        return Subscribe::query()->where('thread_id', $thread_id)->pluck('user_id')->all();
+    }
+}
